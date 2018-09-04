c548434fb96767172e164d1b1108bf0c84fe9c11
AndreMorandi <andreopbmo@gmail.com>
2018-09-04 20:17:03 -0300

    Determine main flexbox containers

--- browse.php
+++ browse.php
@@ -10,19 +10,32 @@ else {
 	header("Location: login.php");
 }
 
-//include("includes/nowPlayingBar.php");
-
 ?>
 
 
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8">
+	<meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport">
 	<title>Browse - Spotify</title>
 	<link rel="icon" type="text/css" href="assets/images/icons/favicon.png">
 	<link rel="stylesheet" type="text/css" href="assets/css/browse.css">
+
 </head>
 <body>
+	<nav class="col-1">
+		Navegação
+	</nav>
+	<div class="col-2">
+	  	<header>Cabeçalho</header>
+	    <main class="content">
+	    <article>Conteúdo</article>
+	    </main>
+	</div>
+
+
+
 
 </body>
 </html>
